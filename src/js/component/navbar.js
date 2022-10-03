19fcf384331fe92c8d97e03200c90d51ea43b6c0
juacomiranda <jmiranda@live.cl>
2022-10-03 17:36:03 +0000

    pre nav listo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,44 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logoimagen from "../../img/logo.jpg"
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1 ms-3">Brain Manager</span>
-			</Link>
-			<div className="ml-auto position-absolute start-50 translate-middle-x">
-				<Link to="/demo">
+			<div className="container-fluid">
+				<Link to="/">
+					<div className="navbar-brand">
+      					<img src={logoimagen} width="30" height="24" className="d-inline-block align-text-top" />
+						<a className="ms-3">App</a>
+    				</div>
+				</Link>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       				<span className="navbar-toggler-icon"></span>
     			</button>
 				<div className="collapse navbar-collapse" id="navbarNav">
-    			  <ul className="navbar-nav">
-        			<li className="nav-item">
-          				<a className="nav-link active" aria-current="page" href="#">Home</a>
-        			</li>
-        			<li className="nav-item">
-          				<a className="nav-link" href="#">Login</a>
-        			</li>
-        			<li className="nav-item">
-          				<a className="nav-link" href="#">Tareas</a>
-        			</li>
-        			<li className="nav-item">
-          				<a className="nav-link">Pomodoro</a>
-        			</li>
-        			<li className="nav-item">
-          				<a className="nav-link">Planner</a>
-        			</li>
-					<li className="nav-item">
-          				<a className="nav-link">Reproductor</a>
-        			</li>
-					<li className="nav-item">
-          				<a className="nav-link">Cuaderno de animo</a>
-        			</li>
-      			</ul>
-    		</div>	
-				</Link>
-			</div>
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<a className="nav-link active" aria-current="page">Home</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link active">Login</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link active">Tareas</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link active">Pomodoro</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link active">Planner</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link active">Reproductor</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link active">Cuaderno de animo</a>
+						</li>
+					</ul>
+				</div>	
+			</div>	
 		</nav>
 	);
 };
