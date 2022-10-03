e48efe0250c1e3b9fc593ee7bb0ea0f062330505
AgustinaRSoria <agustnasoria@hotmail.com>
2022-10-03 20:06:44 +0000

    home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,14 +2,17 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+export const Home = () => { 
+	return (  
+	 <div className="text-center mt-5">
+			<Navbar/>
+		<button>Empecemos</button>
+		<h1>Brain Manager</h1>
+		<p>Aenean molestie accumsan lorem eget luctus. Praesent quis elit vel erat dignissim malesuada vel eu felis. Duis a rhoncus velit, vel vehicula tellus. Integer risus nibh, varius in blandit a, aliquam at justo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+		<userExperience/>
+	<Footer/>
 	</div>
-);
+
+	)
+	};
+export default Home;
