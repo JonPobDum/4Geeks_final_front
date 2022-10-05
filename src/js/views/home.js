03e73bcb7bc97229e100ed66b0b0d3e5b72df82c
juacomiranda <jmiranda@live.cl>
2022-10-05 03:26:08 +0000

    inicio home

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,10 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+// import imageresponsive from "../../img/";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+	<div className="container-fluid m-0">
+		<img src="https://static.timesofisrael.com/www/uploads/2022/04/iStock-1325376146.jpg" className="img-fluid" alt="..." />
 	</div>
 );
