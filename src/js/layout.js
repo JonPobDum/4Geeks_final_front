827bcbd33a886437102a25fe63296e45e1980530
LuisL <luislunandroid@gmail.com>
2022-10-07 03:47:11 +0000

    version 2.2

--- src/js/layout.js
+++ src/js/layout.js
@@ -14,6 +14,7 @@ import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import { Carousel } from "./component/carousel";
 
+
 //create your first component
 export const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
