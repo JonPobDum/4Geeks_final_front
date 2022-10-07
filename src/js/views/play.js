9d8bc2fd48e543889c6c18e064e9381a0a3f1318
juacomiranda <jmiranda@live.cl>
2022-10-07 03:53:27 +0000

    borrar

--- src/js/views/play.js
+++ src/js/views/play.js
@@ -3,9 +3,7 @@ import "../../styles/play.css";
 
 export const Play = () => (
     <div className="container">
-        <div className="play_move">
-            <div className="circulo"></div>
-        </div>
+       Hola
         
     </div>
 )
