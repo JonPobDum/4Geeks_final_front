c79ae2fc807b256ecd8eaa0607ebd75251255795
LuisL <luislunandroid@gmail.com>
2022-10-07 02:36:27 +0000

    version 2.1

--- src/js/layout.js
+++ src/js/layout.js
@@ -7,13 +7,15 @@ import { Login } from "./views/login";
 import { Register} from "./views/register";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
+import { Slideshow } from "./views/carousel";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Carousel } from "./component/carousel";
 
 //create your first component
-const Layout = () => {
+export const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
@@ -23,10 +25,16 @@ const Layout = () => {
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
 					<Navbar />
+					<Carousel />
 					<Switch>
 						<Route exact path="/">
 							<Home />
 						</Route>
+
+						<Route exact path="/carousel">
+							<Slideshow />
+						</Route>
+
 						<Route exact path="/login">
 							<Login />
 						</Route>
