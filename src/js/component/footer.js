d539af5a8f30b50fb810ca88010a03f7d401befa
juacomiranda <jmiranda@live.cl>
2022-10-09 11:25:26 +0000

    Foter v.1

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+	<footer className="footer mt-auto py-3 text-center bg-dark text-light">
+		<img src="https://imagizer.imageshack.com/img924/4281/wcfmDQ.png" />
+		<p> @Copyrigth 2022 by Tinky Winky Team </p>
+		<p> PartTime 28 <a className="text-decoration-none align-self-center" href="https://4geeksacademy.com/es/contactanos?utm_source=google&utm_medium=cpc&utm_campaign=11747537031&utm_content=116275065017&utm_term=4geeks&gclid=Cj0KCQjw-fmZBhDtARIsAH6H8qjkJnbzJdzJtAGsWAc_fp2ZlaMnmtGs4Xle6LfkDb6EkjIE8bLgCkEaAlAKEALw_wcB">4Geeks Academy</a>
 		</p>
 	</footer>
 );
