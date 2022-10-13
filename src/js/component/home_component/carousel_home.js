5068babb54829ef7eb3a78564e4431f7d9e77547
juacomiranda <jmiranda@live.cl>
2022-10-13 19:05:21 +0000

    cambio de imagen home

--- src/js/component/home_component/carousel_home.js
+++ src/js/component/home_component/carousel_home.js
@@ -9,7 +9,7 @@ export const Carousel_home = () => {
 				    <img src="https://static.timesofisrael.com/www/uploads/2022/04/iStock-1325376146.jpg" className="d-block w-100" alt="..." />
 			    </div>	
 			    <div className="carousel-item">
-				    <img src="https://thumbs.dreamstime.com/b/manager-looking-to-wall-overloaded-brain-concept-angry-drawn-147071980.jpg" className="d-block w-100" alt="..." />
+				    <img src="https://images2-mega.cdn.mdstrm.com/meganoticias/2022/03/08/deficit-atencional-adultos_370109_1_622791bb651c5.jpg?d=950x535" className="d-block w-100" alt="..." />
 			    </div>	
 			    <div className="carousel-item">
 				    <img src="https://bodynbrainbucket.s3.amazonaws.com/uploads/newsimage/CenterNews/BNB_Generic/colored-brain.jpg" className="d-block w-100" alt="..." />
