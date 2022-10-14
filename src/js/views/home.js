131652593fb3bb7758f8e48a779029146f9876de
juacomiranda <jmiranda@live.cl>
2022-10-14 21:35:38 +0000

    estimonios

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,12 +3,15 @@ import "../../styles/home.css";
 import { Carousel_home } from "../component/home_component/carousel_home";
 import { Pomodoro } from "../component/home_component/pomodoro_home";
 import { Testimonios } from "../component/home_component/testimonios_home";
+import { Testimonios2 } from "../component/home_component/testimonios_home";
 import { Todo_list } from "../component/home_component/todolist_home";
 // import imageresponsive from "../../img/";
 
 export const Home = () => (
 	<>
 		<Carousel_home />
+		<Testimonios />
+		<Testimonios2 />
 		<Testimonios />
 		<Pomodoro />
 		<Todo_list />
