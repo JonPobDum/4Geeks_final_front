9c2ffb7ee1c7be0d8283daefe959dda316bdbd27
marcelorodriguezbolados <marcelofabianrb@gmail.com>
2022-10-15 23:39:42 +0000

    FETCH EN FLUX, FALTA CAPTURAR VALOR

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,49 +1,72 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			login: (correo, contraseña) => {
-				/**
+  return {
+    store: {
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
+
+      login: (correo, contraseña) => {
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({
+          email: correo,
+          password: contraseña,
+        });
+
+        var requestOptions = {
+          method: "POST",
+          headers: myHeaders,
+          body: raw,
+          redirect: "follow",
+        };
+
+        fetch(
+          "https://3000-jonpobdum-4geeksfinalba-21srwcgl2gr.ws-us71.gitpod.io/api/login",
+          requestOptions
+        )
+          .then((response) => response.json())
+          .then((result) => localStorage.setItem("token", result.token))
+          .catch((error) => console.log("error", error));
+        /**
 					fetch().then().then(data => setStore({ "foo": data.bar }))
 				*/
-				//localStorage.setItem("token", result.token <- viene de la api)
-				console.log(correo, contraseña)
+        //localStorage.setItem("token", result.token <- viene de la api)
+        console.log(correo, contraseña);
+      },
 
 
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+        //reset the global store
+        setStore({ demo: demo });
+      },
+    },
+  };
 };
 
 export default getState;
