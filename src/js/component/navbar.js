06cde9f15239904975c9898d6cbe044ef9a9bec9
juacomiranda <jmiranda@live.cl>
2022-10-16 20:11:12 +0000

    navbar sin color en textos

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,41 +4,32 @@ import logoimagen from "../../img/logo.jpg"
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+		<nav className="navbar navbar-expand-lg bg-dark">
 			<div className="container-fluid">
 				<Link to="/">
 					<div className="navbar-brand">
       					<img src={logoimagen} width="30" height="24" className="d-inline-block align-text-top" />
-						<a className="ms-3">App</a>
+						<a className="ms-3">Brain Manager</a>
     				</div>
 				</Link>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       				<span className="navbar-toggler-icon"></span>
     			</button>
 				<div className="collapse navbar-collapse" id="navbarNav">
-					<ul className="navbar-nav">
-						<li className="nav-item">
-							<a className="nav-link active" aria-current="page">Home</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link active">Login</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link active">Tareas</a>
-						</li>
+					<a className="nav-link" aria-current="page" href="#">Pomodoro</a>
+					<a className="nav-link" aria-current="page" href="#">Agenda</a>
+					<a className="nav-link" aria-current="page" href="#">Reproductor</a>
+					{/* <ul className="navbar-nav ">
 						<li className="nav-item">
 							<a className="nav-link active">Pomodoro</a>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link active">Planner</a>
+							<a className="nav-link active">Agenda</a>
 						</li>
 						<li className="nav-item">
 							<a className="nav-link active">Reproductor</a>
 						</li>
-						<li className="nav-item">
-							<a className="nav-link active">Cuaderno de animo</a>
-						</li>
-					</ul>
+					</ul> */}
 				</div>	
 			</div>	
 		</nav>
