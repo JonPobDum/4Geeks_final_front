47cc53c6c4ecf5db124f99d5407af995e0778035
LuisL <luislunandroid@gmail.com>
2022-10-17 18:00:53 +0000

    version revisada con actualizacion de imagenes en ImageShack

--- src/js/component/carousel.js
+++ src/js/component/carousel.js
@@ -15,24 +15,24 @@ export const Carousel  = () =>{
                 </div>
             <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <img src="https://imagizer.imageshack.com/img922/4594/7EUVYp.png" className="d-block w-100" alt="To Do List image"/>
+                        <img src="https://imagizer.imageshack.com/img922/808/GEBaSR.png" className="d-block w-100" alt="To Do List image"/>
                     </div>
 
                     <div className="carousel-item">
-                        <img src="https://imagizer.imageshack.com/img924/6793/vZZG30.png" className="d-block w-100" alt="Focus image"/>
-                    </div>
+                        <img src="https://imagizer.imageshack.com/img922/9371/5nAEie.png" className="d-block w-100" alt="Focus image"/>
+                    </div> 
 
                     <div className="carousel-item">
-                        <img src="https://imagizer.imageshack.com/img922/2292/uPd1dd.png" className="d-block w-100" alt="Relax image"/>
-                    </div>
+                        <img src="https://imagizer.imageshack.com/img923/8467/wZXfgc.png" className="d-block w-100" alt="Relax image"/>
+                    </div> 
 
                     <div className="carousel-item">
-                        <img src="https://imagizer.imageshack.com/img924/6034/2bdGIn.png" className="d-block w-100" alt="Sleep image"/>
-                    </div>
+                        <img src="https://imagizer.imageshack.com/img924/4411/KeXrN3.png" className="d-block w-100" alt="Sleep image"/>
+                    </div> 
 
                     <div className="carousel-item">
-                        <img src="https://imagizer.imageshack.com/img923/4583/Ispj74.png" className="d-block w-100" alt="Meditation image"/>
-                    </div>
+                        <img src="https://imagizer.imageshack.com/img924/6797/s19s1H.png" className="d-block w-100" alt="Meditation image"/>
+                    </div> 
 
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
--- src/js/component/curtain.js
+++ src/js/component/curtain.js
@@ -9,11 +9,11 @@ export const Curtain  = () =>{
                 Choose your activity
             </h1>
            <div class="gallery">
-                <img src="https://imagizer.imageshack.com/img922/4594/7EUVYp.png" alt="To Do List"/>
-                <img src="https://imagizer.imageshack.com/img924/6793/vZZG30.png" alt="Focus Session"/>
-                <img src="https://imagizer.imageshack.com/img922/2292/uPd1dd.png" alt="Relax Session"/>
-                <img src="https://imagizer.imageshack.com/img924/6034/2bdGIn.png" alt="Sleep Session"/>
-                <img src="https://imagizer.imageshack.com/img923/4583/Ispj74.png" alt="Meditation Session"/>
+                <img src="https://imagizer.imageshack.com/img922/808/GEBaSR.png" alt="To Do List"/>
+                <img src="https://imagizer.imageshack.com/img922/9371/5nAEie.png" alt="Focus Session"/>
+                <img src="https://imagizer.imageshack.com/img923/8467/wZXfgc.png" alt="Relax Session"/>
+                <img src="https://imagizer.imageshack.com/img924/4411/KeXrN3.png" alt="Sleep Session"/>
+                <img src="https://imagizer.imageshack.com/img924/6797/s19s1H.png" alt="Meditation Session"/>
             </div>
         </div>
    
