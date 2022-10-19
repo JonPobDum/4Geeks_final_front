ad53d3f17bed354437cf5522851f0ab0876482bc
LuisL <luislunandroid@gmail.com>
2022-10-19 22:25:50 +0000

    version de analisis de codigo de Pomodoro Circle-Countdown con Seting Slider

---
+++ src/js/component/circle_pomodoro_timer/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import Timer from "../circle_pomodoro_timer/Timer";
+import Settings from "../circle_pomodoro_timer/Settings";
+import {useState} from "react";
+import SettingsContext from "./SettingsContext";
+import { faParachuteBox } from '@fortawesome/free-solid-svg-icons';
+
+function App() {
+    
+    // Declarar los UseState para mostrar o Minutos de Trabajo/Descanzo ó el Timer
+    // Este es el Usestate inicial el Setting
+  const [showSettings, setShowSettings] = useState(false);
+    // Este es el UseState para poder deslizar el boton de Work time, en los sliders empezando con un default de 45 minutos
+  const [workMinutes, setWorkMinutes] = useState(45);
+  // Este es el UseState para poder deslizar el boton de Rest time en los sliders empezando con un default de 15 minutos
+  const [breakMinutes, setBreakMinutes] = useState(15);
+
+  return (
+    <main>
+      <SettingsContext.Provider value={{
+        // El UseState para llamar a la funcion de Settings
+        showSettings,
+        // El UseState mostrar el componente de Settings
+        setShowSettings,
+        // El UseState para llamar a la funcion de workMinutes
+        workMinutes,
+        // El UseState para llamar a la funcion de breakMinutes
+        breakMinutes,
+        // El UseState mostrar el componente del Timer de trabajo
+        setWorkMinutes,
+        // El UseState mostrar el componente del Timer de descanzo
+        setBreakMinutes,
+      }}>
+        {/* Ternario de si muestra Settings o de lo contrario muestra el Timer */}
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/js/component/circle_pomodoro_timer/Settings.js
@@ -0,0 +1,60 @@
+import ReactSlider from 'react-slider';
+import '../../../styles/slider.css'
+import SettingsContext from "./SettingsContext";
+import {useContext} from "react";
+import BackButton from "./BackButton";
+
+// ATENCION: hay que instalar los componentes de controles deslizables con: yarn add react-slider
+function Settings() {
+    // Esta constante se usa para pasar los valores de los sliders al javascript de App.js
+  const settingsInfo = useContext(SettingsContext);
+  return(
+    
+    // Estilos para posicionar los Settings de Work/BreaK
+    <div style={{textAlign:'left'}}>
+      
+      {/* Titulo de controles de trabajo */}
+       {/* el :00 es para mostrar que son minutos exactos */}
+      <label>work: {settingsInfo.workMinutes}:00</label>
+      
+      {/* detalles del componenete Slider */}
+      <ReactSlider
+        className={'slider'}
+        thumbClassName={'thumb'}
+        trackClassName={'track'}
+        // variable que recoge el valor de los minutos de trabajo
+        value={settingsInfo.workMinutes}
+        // funcionalidad para poder deslizar el boton en los slider y que lo tome la funcion del contador
+        onChange={newValue => settingsInfo.setWorkMinutes(newValue)}
+        // minimo valor de trabajo
+        min={15}
+         // máximo valor de trabajo
+        max={90}
+      />
+
+      {/* Titulo de controles de descanzo */}
+      {/* el :00 es para mostrar que son minutos exactos */}
+      <label>break: {settingsInfo.breakMinutes}:00</label>
+      <ReactSlider
+
+        // clase para el boton de descanzo
+        className={'slider green'}
+        thumbClassName={'thumb'}
+        trackClassName={'track'}
+        // variable que recoge el valor de los minutos de descanzo
+        value={settingsInfo.breakMinutes}
+        onChange={newValue => settingsInfo.setBreakMinutes(newValue)}
+        // minimo valor de descanzo
+        min={1}
+        // clase para el boton de descanzo
+        max={120}
+      />
+      <div style={{textAlign:'center', marginTop:'20px'}}>
+        <BackButton onClick={() => settingsInfo.setShowSettings(false)} />
+      </div>
+
+    </div>
+  );
+}
+
+export default Settings;
