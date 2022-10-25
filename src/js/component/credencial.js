4cde82c18df4da88f0b7db828814e7241c7cc9f3
marcelorodriguezbolados <marcelofabianrb@gmail.com>
2022-10-25 01:03:50 +0000

    cambio color fondo, funcion date(), 3 botones

--- src/js/component/credencial.js
+++ src/js/component/credencial.js
@@ -2,59 +2,64 @@ import React from "react";
 import "../../styles/credencial.css";
 
 function Credencial(props) {
-  
+  let now = new Date();
+
   return (
-    <div class="container mt-4 mb-4 p-3 d-flex justify-content-center">
-      <div class="card p-4">
-        <div class=" image d-flex flex-column justify-content-center align-items-center">
-          <button class="btn btn-secondary">
+    <div className="container mt-4 mb-4 p-3 d-flex justify-content-center">
+      <div className="card p-4">
+        <div className=" image d-flex flex-column justify-content-center align-items-center">
+          {/* ESTE ES EL CIRCULO DONDE ESTA LA IMAGEN Y EL COLOR ESTA EN BOOSTRAP */}
+          <button className="btn btn-secondary">
             {" "}
             <img
-              src="https://i.imgur.com/wvxPV9S.png"
+              src="https://i.imgur.com/wvxPV9S.png"image
               height="100"
               width="100"
             />
           </button>
-          <span class="name mt-3"></span>
-          <span class="idd">@NOMBRE_USUARIO</span>{" "}
-          <div class="d-flex flex-row justify-content-center align-items-center gap-2">
-            <span class="idd1">correo_usuario</span>
+          <span className="name mt-3"></span>
+          <span className="idd">@NOMBRE_USUARIO</span>{" "}
+          <div className="d-flex flex-row justify-content-center align-items-center gap-2">
+            <span className="idd1">correo_usuario</span>
             <span>
-              <i class="fa fa-copy"></i>
+              <i className="fa fa-copy"></i>
             </span>
           </div>
-          <div class="d-flex flex-row justify-content-center align-items-center mt-3">
-            <span class="number">
-              1069 <span class="follow">Followers</span>
+          {/* <div className="d-flex flex-row justify-content-center align-items-center mt-3">
+            <span className="number">
+              1069 <span className="follow">Followers</span>
             </span>
-          </div>{" "}
-          <div class=" d-flex mt-2">
-            <button class="btn1 btn-dark"> Go to Carrousel</button>
+          </div>{" "} */}
+          <div className=" d-flex mt-2">
+            <button className="btn1 btn-dark"> Go to my activities </button>
           </div>
-          <div class=" d-flex mt-2">
-            <button class="btn1 btn-dark">log out </button>
+          <div className=" d-flex mt-2">
+            <button className="btn1 btn-dark"> Go to Carrousel</button>
           </div>
-          <div class="text mt-3">
+          <div className=" d-flex mt-2">
+            <button className="btn1 btn-dark">log out </button>
+          </div>
+          <div className="text mt-3">
             <hi>Welcome to brain manager!</hi>
           </div>
-          <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center">
+          <div className="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center">
             {" "}
-            <span>
-              <i class="fa fa-twitter"></i>
+            {/* <span>
+            <i className="fab fa-twitter"></i>
             </span>{" "}
             <span>
-              <i class="fa fa-facebook-f"></i>
+            <i className="fab fa-facebook-f"></i>
             </span>{" "}
             <span>
-              <i class="fa fa-instagram"></i>
+            <i className="fab fa-instagram"></i>
             </span>{" "}
             <span>
-              <i class="fa fa-linkedin"></i>
-            </span>{" "}
+            <i className="fab fa-linkedin-in"></i>
+            </span>{" "} */}
           </div>{" "}
-          <div class=" px-2 rounded mt-4 date ">
+          <div className=" px-2 rounded mt-4 date ">
             {" "}
-            <span class="join">Joined May,2021</span>{" "}
+            <span className="join">{now.toDateString()}</span>{" "}
           </div>{" "}
         </div>{" "}
       </div>
