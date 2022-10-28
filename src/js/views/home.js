9bd982adb73c4f5e8197b8b69b9d68c321f4c889
marcelorodriguezbolados <marcelofabianrb@gmail.com>
2022-10-28 14:43:09 +0000

    tiene home, falta estilos

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,57 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+// import imageresponsive from "../../img/";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+
+	return  <div class="container w-75 mt-5">
+				<div className="row align-items-stretch">
+					<div class="col d-none d-lg-block col-sm-12 col-md-5 col-xl-6 position-relative">
+						<img src="https://imagizer.imageshack.com/img923/5812/J6VbIM.png" className="img-fluid" alt="..." />                    
+					</div>
+					<div class="col bg">
+						<div className="row">
+							<div className="row align-items-stretch">
+								<div className="col">
+									<p className="text-right">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+								</div>	
+								<div className="col-2">
+										<img src="https://cdn0.iconfinder.com/data/icons/brain-activity-flat-style/150/thinking-512.png" alt="..." className="img_1"/>
+								</div>
+							</div>
+								<div className="row">
+									<div className="row align-items-stretch">
+										<div className="col-2">
+											<img src="https://cdn0.iconfinder.com/data/icons/brain-activity-flat-style/150/thinking-512.png" alt="..." className="img_1"/>
+										</div>
+									<div className="col">
+										<p className="text-left">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+									</div>	
+							<div className="row align-items-stretch">
+								<div className="col">
+									<p className="text-left">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+								<div className="col">
+										<img src="https://cdn0.iconfinder.com/data/icons/brain-activity-flat-style/150/thinking-512.png" alt="..." className="img_1"/>
+								</div>
+							</div>
+						</div>
+					</div>		
+				</div>	
+			</div>
+			</div>
+			</div>
+			
+		<div className="row align-items-stretch">
+			<div className="d-none d-lg-block col-sm-12 col-md-5 col-xl-6 col container bg-secondary text-light rounded mt-5">
+				<p>Jane Doe:</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium magna sit amet nibh vestibulum, vel pulvinar libero posuere. Praesent finibus leo libero. Ut mattis lorem sit amet nisl blandit vestibulum. Morbi sollicitudin orci eget odio feugiat, eu scelerisque massa lacinia. Maecenas ornare, est molestie aliquam tristique</p>
+			</div>
+			<div className="d-none d-lg-block col-sm-12 col-md-5 col-xl-6 col container bg-secondary text-light rounded mt-5">
+			<p>John Doe:</p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pretium magna sit amet nibh vestibulum, vel pulvinar libero posuere. Praesent finibus leo libero. Ut mattis lorem sit amet nisl blandit vestibulum. Morbi sollicitudin orci eget odio feugiat, eu scelerisque massa lacinia. Maecenas ornare, est molestie aliquam tristique</p>
+			</div>
+		</div>
+</div>
+
+};
