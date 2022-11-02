b0630b880298ea144dd11b351317bccd2703a4d2
LuisL <luislunandroid@gmail.com>
2022-11-02 22:07:06 +0000

    versión revisada linkeada a las ramas

--- src/js/component/carousel.js
+++ src/js/component/carousel.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import "../../styles/carousel.css"
 
 export const Carousel  = () =>{
+    
+    // Se usó una función que creamos goToUrl para poder ir a otra rama
+    const history = useHistory()
+    const goToUrl = url=>history.push(url)
     return(
         <body>
               <h1 className="h1_carousel">
@@ -19,23 +23,25 @@ export const Carousel  = () =>{
                 </div>
             <div className="carousel-inner">
                     <div className="carousel-item active rounded-2">
-                        <img src="https://imagizer.imageshack.com/img924/1715/7bXGv6.png" className="d-block w-100" alt="To Do List image"/>
+                        
+                        {/* Insertamos la clase onClick para insertar la funcion goToUrl llamando a la rama correspondiente */}
+                        <img onClick= {()=>goToUrl("/todolist")} src="https://imagizer.imageshack.com/img924/1715/7bXGv6.png" className="d-block w-100" alt="To Do List image"/>
                     </div>
 
                     <div className="carousel-item rounded-2">
-                        <img src="https://imagizer.imageshack.com/img923/7686/3htzBE.png" className="d-block w-100" alt="Focus image"/>
+                        <img onClick={()=>goToUrl("/pomodoro")}  src="https://imagizer.imageshack.com/img923/7686/3htzBE.png" className="d-block w-100" alt="Focus image"/>
                     </div> 
 
                     <div className="carousel-item rounded-2">
-                        <img src="https://imagizer.imageshack.com/img922/6886/5wcFG4.png" className="d-block w-100" alt="Relax image"/>
+                        <img onClick= {()=>goToUrl("/playermode")} src="https://imagizer.imageshack.com/img922/6886/5wcFG4.png" className="d-block w-100" alt="Relax image"/>
                     </div> 
 
                     <div className="carousel-item rounded-4">
-                        <img src="https://imagizer.imageshack.com/img923/1672/ulqDeP.png" className="d-block w-100" alt="Sleep image"/>
+                        <img onClick= {()=>goToUrl("/playermode")} src="https://imagizer.imageshack.com/img923/1672/ulqDeP.png" className="d-block w-100" alt="Sleep image"/>
                     </div> 
 
                     <div className="carousel-item rounded-4">
-                        <img src="https://imagizer.imageshack.com/img924/88/VnfrbR.png" className="d-block w-100" alt="Meditation image"/>
+                        <img onClick= {()=>goToUrl("/playermode")} src="https://imagizer.imageshack.com/img924/88/VnfrbR.png" className="d-block w-100" alt="Meditation image"/>
                     </div> 
 
             </div>
