9242b4dc99a30c7649ed343d5a7262a12071c237
LuisL <luislunandroid@gmail.com>
2022-11-04 23:37:36 +0000

    version con el Navbar arreglado y empezado a arreglar el footer

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,73 +1,45 @@
 import React,{useEffect,useContext} from "react";
 import { Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
-import { Context } from "../store/appContext";
-import logoimagen from "../../img/logo.jpg"
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
 
-	// const  history = useHistory();
-
-    // useEffect(()=>{
-        
-    //     if(localStorage.getItem("Token")){
-    //       alert("welcome")
-    //       }
-    //   else{
-    //       history.push("/")
-    //   }
-        
-    //   },[])
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+		<nav className="navbar navbar-expand-lg navbar-light bg-dark">
 			<div className="container-fluid">
-				<Link to="/">
-					<div className="navbar-brand">
-      					{/* <img src={logoimagen} width="30" height="24" className="d-inline-block align-text-top" /> */}
-						<a className="ms-3">Home</a>
-    				</div>
-				</Link>
+				
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       				<span className="navbar-toggler-icon"></span>
     			</button>
 				<div className="collapse navbar-collapse" id="navbarNav">
 					<ul className="navbar-nav">
-						{/* <li className="nav-item">
-							<a className="nav-link active" aria-current="page">Home</a>
-						</li> */}
-						<li className="nav-item">
-						<Link to="/login">
-							<a className="nav-link active">Login</a>
-						</Link>
+					<li className="nav-item">
+							<Link to="/">
+								<div className="navbar-brand">
+									<img src="https://imagizer.imageshack.com/img924/9654/35j7ZL.png" width="60" height="60" className="" />
+									<button class="no-border btn btn-outline-light ms-2 m120" type="button">Home</button>
+								</div>
+							</Link>
 						</li>
 						<li className="nav-item">
-						<Link to="/pomodoro">
-							<a className="nav-link active">Timer</a>
-						</Link>
-						
-						</li>
-						<li className="nav-item">
-						<Link to="/carousel">
-							<a className="nav-link active">carousel</a>
-						</Link>
-						</li>
-						<li className="nav-item">
-						<Link to="/todolist">
-							<a className="nav-link active">TodoList</a>
-						</Link>
-						</li>
-						<li className="nav-item">
-						<Link to="/perfil">
-							<a className="nav-link active">Profile</a>
-						</Link>
-						{/* <Link to="/">
-							<a className="nav-link active">Player</a>
-						</Link> */}
-						</li>
-						<li className="nav-item">
-						
+							<Link to="/login">
+								<button class="btn btn-outline-light no-border me-2 m120 m-top" type="button">Login</button>
+								
+							</Link>
 						</li>
 						
+						<li className="nav-item">
+							<Link to="/carousel">
+								<button class="btn btn-outline-light no-border me-2 m240 m-top" type="button">Choose your Activity</button>
+							</Link>
+						</li>
+					
+						<li className="nav-item">
+							<Link to="/perfil">
+								<button class="btn btn-outline-light no-border me-2 m120 m-top" type="button">Profile</button>	
+							</Link>
+						</li>
+
 					</ul>
 				</div>	
 			</div>	
