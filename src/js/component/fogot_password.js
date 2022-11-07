cd934f914048d1705576ecf097ca0e069a07695d
LuisL <luislunandroid@gmail.com>
2022-11-07 19:56:30 +0000

    version terminada de forgot password

--- src/js/component/fogot_password.js
+++ src/js/component/fogot_password.js
@@ -18,33 +18,32 @@ const sendEmail =(event) =>{
       return(
             <div class="container w-75 mt-5">
   
-            <div className="row align-items-stretch">
-                         
-                            <div class="col d-none d-lg-block col-sm-12 col-md-5 col-xl-6 position-relative">
-                                  <img class="img-fluid position-absolute top-0 end-0 bg2 r1" src="https://imagizer.imageshack.com/img924/2575/XCBZCz.png" alt="Login image"></img>
-                            </div> 
-            
-            
-            <div className="form_fp">
-                  <form   id="form" style={{ width: "38rem" }} onSubmit={sendEmail}>
-                        <img src="https://imagizer.imageshack.com/img924/6517/IZAfXn.png" height="150px" className="mx-auto d-block"/>
-                        <div class="mb-3">
-                              <label for="exampleInputEmail1" class="form-label" style={{color: "white"}}>Name</label>
-                              <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required  name='name'></input>
-                              <div id="emailHelp" class="form-text" style={{color: "white"}}></div>
-                        </div>
+                  <div className="row align-items-stretch">
+                              
+                              <div class="col d-none d-lg-block col-sm-12 col-md-5 col-xl-6 position-relative">
+                                    <img class="img-fluid position-absolute top-0 end-0 bg2 r1" src="https://imagizer.imageshack.com/img924/2575/XCBZCz.png" alt="Login image"></img>
+                              </div> 
+                  
+                  <div className="form_fp">
+                        <form   id="form" style={{ width: "auto" }} onSubmit={sendEmail}>
+                              <img src="https://imagizer.imageshack.com/img924/6517/IZAfXn.png" height="150px" className="mx-auto mt-2 d-block"/>
+                              <div class="mb-3">
+                                    <label for="exampleInputEmail1" class="form-label" style={{color: "white"}}>Name</label>
+                                          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required  name='name'></input>
+                                    <div id="emailHelp" class="form-text" style={{color: "white"}}></div>
+                              </div>
 
-                        <div className="mb-3">
-                              <label for="exampleInputEmail1" className="form-label" style={{color: "white"}}>Email address</label>
-                              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"  required name='email'></input>
-                              <div id="emailHelp" className="form-text" style={{color: "white"}}>We'll never share your email with anyone else.</div>
-                        </div>
+                              <div className="mb-3">
+                                    <label for="exampleInputEmail1" className="form-label" style={{color: "white"}}>Email address</label>
+                                          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"  required name='email'></input>
+                                    <div id="emailHelp" className="form-text" style={{color: "white"}}>We'll never share your email with anyone else.</div>
+                              </div>
 
-                  {/* <Link to="/login"> */}
-                  <button id='button' type="submit" className="btn btn-primary " style={{ width: "38rem" }}>Ask your password</button>
-                  {/* </Link> */}
-                  </form>
-            </div>
+                        {/* <Link to="/login"> */}
+                        <button id='button' type="submit" className="btn btn-primary mb-5" style={{ width: "80%" }}>Ask your password</button>
+                        {/* </Link> */}
+                        </form>
+                  </div>
       </div>
       </div>
       
