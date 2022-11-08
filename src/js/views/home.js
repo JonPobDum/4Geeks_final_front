3ce959defe091346d2f03e7e5fb0056313327bf4
LuisL <luislunandroid@gmail.com>
2022-11-08 21:08:07 +0000

    version final

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,18 +6,18 @@ export const Home = () => {
 
 	return <div className="container w-75 mt-5">
 									<div className="row align-items-stretch">
-										<div className="col d-none d-lg-block col-sm-12 col-md-5 col-xl-6 position-relative">
-											<img src="https://imagizer.imageshack.com/img923/5812/J6VbIM.png" className="img-fluid bg2" alt="..." />                    
+										<div className="col d-none d-lg-block col-sm-12 col-md-5 col-xl-6 position-relative bg1">
+											<img src="https://imagizer.imageshack.com/img923/5812/J6VbIM.png" className="img-fluid portada" alt="..." />                    
 										</div>
-										<div className="col bg1">
+										<div className="col bg">
 											<div className="row">
 												<div className="row align-items-stretch">
 													<div className="col">
-														<p className="text-right mt-2"><strong><i>Thanks to visit our web page or app</i></strong>. It will be glad that you enjoy our initiative. We have combined mindtools to be more productive than we could.</p>
+														<p className="text-right"><strong><i>Thanks to visit our web page or app</i></strong>. It will be glad that you enjoy our initiative. We have combined mindtools to be more productive than we could.</p>
 														<p>We mix up a Pomodoro timer, Binaurals beats and a planing agenda.</p>
 													</div>	
 													<div className="col-2">
-															<img src="https://imagizer.imageshack.com/img923/6497/95OkLs.png" className="mt-4"alt="..." width={"120px"}/>
+															<img src="https://imagizer.imageshack.com/img923/6497/95OkLs.png" alt="..." className="img_1"/>
 													</div>
 												</div>
 													<div className="row">
@@ -25,22 +25,22 @@ export const Home = () => {
 															<div className="col-2">
 																<img src="https://cdn0.iconfinder.com/data/icons/brain-activity-flat-style/150/thinking-512.png" alt="..." className="img_1"/>
 															</div>
-															<div className="col-10">
-																<p className="ps-text-left ps-4">Pomodoro timer provided us a register of our focus time and relax time to engaged our cognitive skills, due to we can set up those focus and relax period; to let us settledown and fix up our most better work/relax cycle. Furthermore saving our goals to get better and better our mindset profits.</p>
-															</div>	
+														<div className="col">
+															<p className="text-left">Pomodoro timer provided us a register of our focus time and relax time to engaged our cognitive skills, due to we can set up those focus and relax period; to let us settledown and fix up our most better work/relax cycle. Furthermore saving our goals to get better and better our mindset profits.</p>
+														</div>	
 												<div className="row align-items-stretch">
 													<div className="col">
-														<p className="text-left">Aditional of these we provide some radio stations binaureals beats who has the the proposit  an interference pattern between two sounds of slightly different frequencies, perceived as a periodic variation in volume whose rate is the difference of the two frequencies. </p>
+														<p className="text-left ml-2">Aditional of these we provide some radio stations binaureals beats who has the the proposit  an interference pattern between two sounds of slightly different frequencies, perceived as a periodic variation in volume whose rate is the difference of the two frequencies. </p>
 													<div className="col">
-															<p>Neural oscillations, or brainwaves, are rhythmic or repetitive patterns of neural activity in the central nervous system. Neural tissue can generate oscillatory activity in many ways, driven either by mechanisms within individual neurons or by interactions between neurons. At the level of neural ensembles, synchronized activity of large numbers of neurons can give rise to macroscopic oscillations, which can be observed in an electroencephalogram.</p>
-															<p>All of those kind of brainwaves are related to an activity such ass: meditation, concentration, learning, relax, deep sleep, dreams, creativity, etc.</p>
-															<p>A binaural beat is an illusion created by the brain when you listen to two tones with slightly different frequencies at the same time. Your brain interprets the two tones as a beat of its own. The two tones align with your brain waves to produce a beat with a different frequency. This frequency is the difference in hertz (Hz) between the frequencies of the two tones.</p>
-															<p>When you listen to binaural beats, your brain activity matches the frequency set by the frequency of the beat. This is called the frequency-following effect. This means you can use binaural beats to entrain your mind to reach a certain mental state.</p>
-															<img src="https://imagizer.imageshack.com/img923/6182/5MOhlQ.png" alt="..." width={"380px"} />
+															<p className="text-left ml-2">Neural oscillations, or brainwaves, are rhythmic or repetitive patterns of neural activity in the central nervous system. Neural tissue can generate oscillatory activity in many ways, driven either by mechanisms within individual neurons or by interactions between neurons. At the level of neural ensembles, synchronized activity of large numbers of neurons can give rise to macroscopic oscillations, which can be observed in an electroencephalogram.</p>
+															<p className="text-left ml-2">All of those kind of brainwaves are related to an activity such ass: meditation, concentration, learning, relax, deep sleep, dreams, creativity, etc.</p>
+															<p className="text-left ml-2">A binaural beat is an illusion created by the brain when you listen to two tones with slightly different frequencies at the same time. Your brain interprets the two tones as a beat of its own. The two tones align with your brain waves to produce a beat with a different frequency. This frequency is the difference in hertz (Hz) between the frequencies of the two tones.</p>
+															<p className="text-left ml-2">When you listen to binaural beats, your brain activity matches the frequency set by the frequency of the beat. This is called the frequency-following effect. This means you can use binaural beats to entrain your mind to reach a certain mental state.</p>
+															<img src="https://cdn0.iconfinder.com/data/icons/brain-activity-flat-style/150/thinking-512.png" alt="..." className="img_1"/>
 													</div>
 													<div className="col">
-															<p>Some claimed benefits of binaural beats therapy may include: reduced stress, reduced anxiety, increased focus, increased concentration, increased motivation, increased confidence, and deeper meditation.</p>
-															<img src="https://www.digitaltrends.com/wp-content/uploads/2012/08/eeg-headset1.jpg?p=1" alt="..." className="img_1" width={"380px"} className="rounded"/>
+															<p className="text-left ml-2">Some claimed benefits of binaural beats therapy may include: reduced stress, reduced anxiety, increased focus, increased concentration, increased motivation, increased confidence, and deeper meditation.</p>
+															<img src="https://cdn0.iconfinder.com/data/icons/brain-activity-flat-style/150/thinking-512.png" alt="..." className="img_1"/>
 															<p>We aspire that you use this tools everyday to be sharp about your concentration and productive performance.</p>
 													</div>
 												</div>
@@ -52,24 +52,16 @@ export const Home = () => {
 								</div>
 								
 							<div className="row align-items-stretch">
-									
-								<div className="d-none d-lg-block col-sm-12 col-md-5 col-xl-6 col container opinions text-light rounded mt-5 p-2">
-									<div>
-										<img src="https://imagizer.imageshack.com/img923/9494/VGWO94.png" width={"60px"} class="rounded-circle"/>
-									</div>
-									
-										<p>Jane Doe:</p>
-										<p>“.... I am very passionate about being able to sharp my mindset. Most people need some apps like this to dont waste our time.... Regards.”</p>
-									</div>
-								
-								<div className="d-none d-lg-block col-sm-12 col-md-5 col-xl-6 col container opinions text-light rounded mt-5 p-2 p">
-								
-									<div>
-										<img src="https://imagizer.imageshack.com/img923/1046/DItVkt.png" width={"60px"} class="rounded-circle"/>
-									</div>
-										<p>Pedro Pérez:</p>
-										<p>“....Got this app for me and i love it, actually my jobmates asking me what am i doing. They realice my attention and fast respouns to solve problems. It’s my little secret.... Well done guys!” .</p>
-									</div>
+								<div className="d-none d-lg-block col-sm-12 col-md-5 col-xl-6 col container bg-secondary text-light rounded mt-5">
+									<img src="https://imagizer.imageshack.com/img923/9494/VGWO94.png" width={"60px"} className="rounded-circle"/>
+									<p>Jane Doe:</p>
+									<p>“.... I am very passionate about being able to sharp my mindset. Most people need some apps like this to dont waste our time.... Regards.”</p>
+								</div>
+								<div className="d-none d-lg-block col-sm-12 col-md-5 col-xl-6 col container bg-secondary text-light rounded mt-5">
+								<img src="https://imagizer.imageshack.com/img923/1046/DItVkt.png" width={"60px"} className="rounded-circle"/>
+								<p>John Doe:</p>
+									<p>“....Got this app for me and i love it, actually my jobmates asking me what am i doing. They realice my attention and fast respouns to solve problems. It’s my little secret.... Well done guys!” .</p>
+								</div>
 							</div>
 					</div>
 };
--- src/js/views/playermode.js
+++ src/js/views/playermode.js
@@ -5,25 +5,30 @@ import "../../styles/player.css"
 export function Applayer() {
   const [songs] = useState([
     {
-      title: "SoundHelix Song",
-      artist: "T. Schürge",
-      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-12.mp3"
+      title: "Relax",
+      artist: "Alpha 8 - 13 Hz",
+      src: "https://freebinaural.com/wp-content/uploads/2020/10/DNA-Repair-Frequency-_-Healing-Theta-Meditation-_-Cell-Regeneration-w_-Binaural-Beats.mp3"
     },
+
     {
-      title: "SoundHelix Song 12",
-      artist: "T. Schürge",
-      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+      title: "Sleep",
+      artist: "Deltha 0, 4Hz",
+      src: "https://freebinaural.com/wp-content/uploads/2020/09/Delta-Binaural-Beat.mp3"
     },
+
     {
-      title: "SoundHelix Song 15",
-      artist: "T. Schürge",
-      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-15.mp3"
+      title: "Meditation",
+      artist: "13 - 32Hz",
+      src: "https://freebinaural.com/wp-content/uploads/2020/12/Beta-12.5Hz-Free-Binaural-Beats.mp3"
     },
+    
     {
-      title: "SoundHelix Song 16",
-      artist: "T. Schürge",
-      src: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-16.mp3"
-    }
+      title: "Focus",
+      artist: "Gamma 32 - 100Hz",
+      src: "https://freebinaural.com/wp-content/uploads/2020/10/Awake-_-Gamma-Wave-Binaural-Beats-_-Focus-Memory-Studying-_-Relaxation-Healing.mp3"
+    },
+
+    
   ]);
 
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
@@ -39,21 +44,25 @@ export function Applayer() {
     });
   }, [currentSongIndex]);
 
-  return (  <div className="App">
-                  <div className="row align-items-stretch main">
-                        <div className="col d-none d-lg-block col-sm-12 col-md-5 col-xl-6 ">
-                              <img className="img-fluid position-relative" src="https://i.ytimg.com/vi/a3H5hr8OLu8/maxresdefault.jpg"></img>
+  return (  <div className="App position-absolute">
+                  <div className="row img-fluid">
+                        <div className="col d-none d-lg-block col-sm-12 col-md-5 col-xl-6 mt-4">
+                              <img className="img-fluid bg2 position-relative" width="800px" src="https://www.naturline.com/wp-content/uploads/2018/04/Binaural-Beats.jpg"></img>
                         </div>
-                        <div className="col img-2">
-                                <Player className
+                        <div className="col img-2 position-relative">
+                                <Player className=""
                                   currentSongIndex={currentSongIndex} 
                                   setCurrentSongIndex={setCurrentSongIndex} 
                                   nextSongIndex={nextSongIndex} 
                                   songs={songs}
                                 />
                         </div>
+                      
                   </div>
+                
     </div>
     
   );
+
+
 }
